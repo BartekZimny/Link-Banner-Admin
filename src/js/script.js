60ed588424f95dc6ec2d159d5843808e65214cf8
Bartłomiej Zimny <bart.zimny@gmail.com>
2019-11-17 22:54:17 +0100

    Add modals function & quit modal

--- src/js/script.js
+++ src/js/script.js
@@ -39,3 +39,43 @@ for (var j = 0; j < menuPages.length; j++) {
     }
   });
 }
+
+// Modals
+function closeModal() {
+  document.getElementById('overlay').classList.remove('show');
+}
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    closeModal();
+  });
+});
+
+document.querySelector('#overlay').addEventListener('click', function(e) {
+  if (e.target === this) {
+    closeModal();
+  }
+});
+
+document.addEventListener('keyup', function(e) {
+  if (e.keyCode === 27) {
+    closeModal();
+  }
+});
+
+function openModal(modal) {
+  document.querySelectorAll('#overlay > *').forEach(function(modal) {
+    modal.classList.remove('show');
+  });
+  document.querySelector('#overlay').classList.add('show');
+  document.querySelector(modal).classList.add('show');
+}
+
+document.getElementById('go_quit').addEventListener('click', function() {
+  openModal('#modal_quit');
+});
+
+document.getElementById('go_site').addEventListener('click', function() {
+  window.location.replace('https://github.com/TheWizard0f0z/');
+});
