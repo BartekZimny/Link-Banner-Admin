3d8b03333a330de63bde56c32eaf2b59feefc1c9
Bartłomiej Zimny <bart.zimny@gmail.com>
2019-11-18 23:06:35 +0100

    Add interactive chart

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,13 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+'use strict';
+
+// Check screen width (mobile)
+if (window.screen.width < 576) {
+  document.querySelector('.main').classList.add('wide');
+  document.querySelector('.sidebar').classList.add('toggled');
+}
+
 // Hamburger
 function toggleSidebar(visible) {
   document.querySelector('.sidebar').classList.toggle('toggled', visible);
@@ -39,6 +49,44 @@ for (var j = 0; j < menuPages.length; j++) {
     }
   });
 }
+
+// Chart
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+  // 1 (chart type)
+  type: 'bar',
+  data: {
+    // 2 (labels for the X axis)
+    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+    // 3 (data series setting)
+    datasets: [
+      {
+        // 4 (naming the data series)
+        label: 'Signups',
+        // 5 (data series color selection)
+        backgroundColor: '#8DBEC8',
+        borderColor: '#8DBEC8',
+        // 6 (supplement with sample data)
+        data: [346, 211, 232, 362, 419, 391, 289, 269, 278, 196]
+      },
+      {
+        label: 'FTD',
+        backgroundColor: '#F29E4E',
+        borderColor: '#F29E4E',
+        data: [411, 168, 294, 273, 471, 122, 187, 512, 324, 203]
+      },
+      {
+        label: 'Earned',
+        backgroundColor: '#71B374',
+        borderColor: '#71B374',
+        data: [378, 189, 263, 317, 445, 256, 238, 390, 301, 199],
+        // 7
+        hidden: true
+      }
+    ]
+  }
+});
 
 // Modals
 function closeModal() {
